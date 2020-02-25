bbc2791b73b09f50549c53ab514c0c589570cf75
Artur Santos <artursantos30.ars@gmail.com>
2020-02-25 17:09:03 -0400

    Insertion Sort

---
+++ Sorting/InsertionSort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printArray(int v[], int n){
+
+    printf("[ ");
+    for (int i = 0; i < n; i++){
+        printf("%i ",v[i]);
+    }
+    printf("]\n");
+
+}
+
+void insertionSort(int v[], int n){
+
+    int pivot, i, j;
+
+    for (i = 1; i < n; i++){
+        pivot = v[i];
+        j = i - 1;
+        while (pivot < v[j] && j >= 0){
+            v[j + 1] = v[j];
+            j--;          
+        }
+        v[j + 1] = pivot;
+    }
+
+}
+
+int main(){
+    
+    int n, *v;
+
+    printf("Type the size of the Array: ");
+    scanf("%i",&n);
+    v = (int*)malloc(n*sizeof(int));
+
+    printf("Type the elements of the Array: \n");
+    for (int i = 0; i < n; i++){
+        scanf("%i", &v[i]);
+    }
+    
+    insertionSort(v,n);
+    printArray(v,n);
+
+    return 0;
+}
